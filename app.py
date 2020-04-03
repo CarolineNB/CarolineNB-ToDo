f57d87d150a7378d9e76d5e7f2f4334a441f37af
Caroline B <eniloracb4@gmail.com>
2020-04-03 15:00:23 -0600

    Using Jinja to update checkboxes

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ def create_todo():
     body = {}
     try:
         description = request.get_json()['description'] #the second value is default if empty
-        todo = Todo(description2=description)
+        todo = Todo(description=description)
         db.session.add(todo)
         db.session.commit()
         return jsonify({
@@ -45,6 +45,20 @@ def create_todo():
         abort(400)
     else:
         return jsonify(body)
+
+@app.route('/todos/<todo_id>/set-completed', methods=['POST'])
+def set_completed_todo(todo_id):
+    try:
+        completed = request.get_json()['completed']
+        print('completed', completed)
+        todo = Todo.query.get(todo_id)
+        todo.completed = completed
+        db.session.commit()
+    except:
+        db.session.rollback()
+    finally:
+        db.session.close()
+    return redirect(url_for('index'))
 
 @app.route('/')
 def index():
