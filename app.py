8b5e4a6fa5c43f496056b65565e48b66aa7399f9
Caroline B <eniloracb4@gmail.com>
2020-04-04 12:22:30 -0600

    final touches

--- app.py
+++ app.py
@@ -20,6 +20,23 @@ class Todo(db.Model):
     def __repr__(self):
         return f'<Todo {self.id} {self.description}>'
 
+#The following shows how to use a many-to-many relational table
+# order_items = db.Table('order_items', 
+#     db.Column('order_id', db.Integer, db.ForeignKey('order.id'), primary_key=True),
+#     db.Column('product_id', db.Integer, db.ForeignKey('product.id'), primary_key=True)
+# )
+
+# class Order(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     status = db.Column(db.String(), nullable=False)
+#     products = db.relationship('Product', secondary=order_items, 
+#         backref=db.backref('orders'), lazy=True)
+
+# class Product(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     name = db.Column(db.String(), nullable=False)
+
+
 class TodoList(db.Model):
     __tablename__ = 'todolists'
     id = db.Column(db.Integer, primary_key=True)
